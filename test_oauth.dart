993a780e93cf116a8fedcff0cf784bf3e3d10020
Wei Jie Yap <wyap@u91z02s01.42kl.edu.my>
2023-10-06 19:27:41 +0800

    1st commit (setup)

---
+++ test_oauth.dart
@@ -0,0 +1,10 @@
+// https://pub.dev/packages/http
+
+import 'package:http/http.dart' as http
+
+var url = Uri.https('example.com', 'whatsit/create');
+var response = await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+print('Response status: ${response.statusCode}');
+print('Response body: ${response.body}');
+
+print(await http.read(Uri.https('example.com', 'foobar.txt')));
